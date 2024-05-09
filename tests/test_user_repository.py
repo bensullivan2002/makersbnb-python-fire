8535ff83b405d516610e48d516d4de345d426913
James Rumble <jamesrumble@live.com>
2024-05-09 14:44:09 +0100

    User log-in functionality

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -11,16 +11,7 @@ def test_get_user_back_based_on_id(db_connection):
     result = user_repository.find(2)
     assert result == User(2, 'angelica@gmail.com', 'Password567!', 'Angelica', 'Gottlieb', '07895687907')
 
-"""
-When I use check_email_valid I get True if email already exists
-"""
-def test_get_user_back_based_on_email(db_connection):
-    db_connection.seed('seeds/makersbnb_fire.sql')
-    user_repository = UserRepository(db_connection)
-    result = user_repository.check_email_valid('ben@gmail.com')
-    result2 = user_repository.check_email_valid('wrongemail@hotmail.com')
-    assert result == True
-    assert result2 == False
+
 
 """
 When I use find_user_from_email, I get user back with their information
@@ -31,6 +22,13 @@ def test_find_user_from_email(db_connection):
     user_repository = UserRepository(db_connection)
     user = user_repository.find_user_from_email('angelica@gmail.com')
     assert user == User(2, 'angelica@gmail.com', 'Password567!', 'Angelica', 'Gottlieb', '07895687907')
+
+def test_find_user_from_email_doesnt_exist(db_connection):
+    db_connection.seed('seeds/makersbnb_fire.sql')
+    user_repository = UserRepository(db_connection)
+    assert user_repository.find_user_from_email('none@gmail.com') == None
+    
+
 
 """
 When we use login, if username and password match 
@@ -48,12 +46,3 @@ def test_sign_up_adds_database(db_connection):
     users = user_repository.all()
     assert users == [User(1, 'ben@gmail.com', 'Password123!', 'Ben', 'Sullivan', '07223487567'), User(2, 'angelica@gmail.com', 'Password567!', 'Angelica', 'Gottlieb', '07895687907'), User(3, 'james@gmail.com', 'Password555!', 'James', 'Rumble', '07895687912')]
 
-# """
-# When I use check_password_matches_email I get True if password matches email
-# """
-
-# def test_password_matches_email(db_connection):
-#     db_connection.seed('seeds/user_library.sql')
-#     user_repository = UserRepository(db_connection)
-#     password_matches = user_repository.check_password_matches_email('example2@hotmail.com', 'Password12345')
-#     assert password_matches == True
